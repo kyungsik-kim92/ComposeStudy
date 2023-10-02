51c489dc2cb03ea8aad19b91867ac19a22f38b0a
Kim Kyungsik <ksiki1@naver.com>
2023-10-02 23:17:49 +0900

    카드 따라해보기

--- cardpractice/src/main/java/com/example/cardpractice/MainActivity.kt
+++ cardpractice/src/main/java/com/example/cardpractice/MainActivity.kt
@@ -3,13 +3,23 @@ package com.example.cardpractice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.cardpractice.ui.theme.ComposeStudyTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +27,35 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeStudyTheme {
-
+                Card()
             }
         }
     }
 }
 
+@Composable
+fun Card() {
+
+    var cardFront by remember {
+        mutableStateOf(true)
+    }
+
+    val animationDegree by animateFloatAsState(targetValue = if (cardFront) 0f else 180f)
+    Box(
+        Modifier
+            .fillMaxSize()
+            .padding(top = 50.dp, bottom = 50.dp, start = 20.dp, end = 20.dp)
+            .graphicsLayer {
+                rotationY = animationDegree
+            }
+            .clickable {
+                cardFront = !cardFront
+
+            }
+            .background(Color.Gray))
+
+
+}
 
 
 @Preview(showBackground = true)
