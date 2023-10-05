6af0c410db660df4b1e3e577e08d89afd190374a
Kim Kyungsik <ksiki1@naver.com>
2023-10-05 18:26:15 +0900

    shape 한번 적용해보기.

--- themepractice/src/main/java/com/example/themepractice/MainActivity.kt
+++ themepractice/src/main/java/com/example/themepractice/MainActivity.kt
@@ -4,14 +4,20 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -22,146 +28,134 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeStudyTheme(){
-                Test2()
+            ComposeStudyTheme() {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    RadiusTest2()
+                }
+
+            }
+        }
+    }
+
+
+    @Composable
+    fun RadiusTest1() {
+
+        Column() {
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(100.dp)
+                    .padding(20.dp)
+                    .clip(MaterialTheme.shapes.extraLarge)
+                    .background(Color.Red)
+            ) {
+
+            }
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(100.dp)
+                    .padding(20.dp)
+                    .clip(MaterialTheme.shapes.large)
+                    .background(Color.Red)
+            ) {
+
+            }
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(100.dp)
+                    .padding(20.dp)
+                    .clip(MaterialTheme.shapes.medium)
+                    .background(Color.Red)
+            ) {
+
+            }
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(100.dp)
+                    .padding(20.dp)
+                    .clip(MaterialTheme.shapes.small)
+                    .background(Color.Red)
+            ) {
+
+            }
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(100.dp)
+                    .padding(20.dp)
+                    .clip(MaterialTheme.shapes.extraSmall)
+                    .background(Color.Red)
+            ) {
+
             }
 
         }
-    }
-}
 
-
-@Composable
-fun Test1() {
-    Column {
-        Text(
-            text = "여기는 제목",
-            fontSize = 30.sp,
-            fontWeight = FontWeight.ExtraBold,
-            modifier = Modifier.padding(30.dp)
-        )
-
-        Text(
-            text = "여기는 내용", fontSize = 20.sp,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(30.dp)
-        )
-        Text(
-            text = "여기는 하단글", fontSize = 10.sp,
-            fontWeight = FontWeight.SemiBold,
-            modifier = Modifier.padding(30.dp)
-        )
-
-        Text(
-            text = "여기는 제목",
-            style = MaterialTheme.typography.titleLarge,
-            modifier = Modifier.padding(30.dp)
-        )
-        Text(
-            text = "여기는 내용",
-            style = MaterialTheme.typography.titleMedium,
-            modifier = Modifier.padding(30.dp)
-        )
-        Text(
-            text = "여기는 하단글",
-            style = MaterialTheme.typography.titleSmall,
-            modifier = Modifier.padding(30.dp)
-        )
     }
 
-}
+    @Composable
+    fun RadiusTest2() {
+
+        Column() {
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(100.dp)
+                    .padding(20.dp)
+                    .clip(MaterialTheme.shapes.extraLarge)
+                    .background(Color.Blue)
+            ) {
+
+            }
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(100.dp)
+                    .padding(20.dp)
+                    .clip(MaterialTheme.shapes.large)
+                    .background(Color.Blue)
+            ) {
+
+            }
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(100.dp)
+                    .padding(20.dp)
+                    .clip(MaterialTheme.shapes.medium)
+                    .background(Color.Blue)
+            ) {
+
+            }
 
 
-@Composable
-fun Test2(){
-    Column() {
-        Text(
-            text = "Display Large",
-            style = MaterialTheme.typography.displayLarge
-        )
-
-        Text(
-            text = "Display Medium",
-            style = MaterialTheme.typography.displayMedium
-        )
-
-        Text(
-            text = "Display Small",
-            style = MaterialTheme.typography.displaySmall
-        )
-
-        Text(
-            text = "Headline Large",
-            style = MaterialTheme.typography.headlineLarge
-        )
-
-        Text(
-            text = "Headline Medium",
-            style = MaterialTheme.typography.headlineMedium
-        )
-
-        Text(
-            text = "Headline Small",
-            style = MaterialTheme.typography.headlineSmall
-        )
-
-        Text(
-            text = "Title Large",
-            style = MaterialTheme.typography.titleLarge
-        )
-
-        Text(
-            text = "Title Medium",
-            style = MaterialTheme.typography.titleMedium
-        )
-
-        Text(
-            text = "Title Small",
-            style = MaterialTheme.typography.titleSmall
-        )
-
-        Text(
-            text = "Body Large",
-            style = MaterialTheme.typography.bodyLarge
-        )
-
-        Text(
-            text = "Body Medium",
-            style = MaterialTheme.typography.bodyMedium
-        )
-
-        Text(
-            text = "Body Small",
-            style = MaterialTheme.typography.bodySmall
-        )
-
-        Text(
-            text = "Label Large",
-            style = MaterialTheme.typography.labelLarge
-        )
-
-        Text(
-            text = "Label Medium",
-            style = MaterialTheme.typography.labelMedium
-        )
-
-        Text(
-            text = "Label Small",
-            style = MaterialTheme.typography.labelSmall
-        )
+        }
 
     }
 
 
-}
+    @Preview(showBackground = true)
+    @Composable
+    fun TestPreview1() {
+        ComposeStudyTheme() {
+            RadiusTest2()
+        }
 
-
-@Preview(showBackground = true)
-@Composable
-fun TestPreview1() {
-    ComposeStudyTheme(){
-        Test2()
     }
-
 }
 
---
+++ themepractice/src/main/java/com/example/themepractice/ui/theme/Shapes.kt
@@ -0,0 +1,11 @@
+package com.example.themepractice.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val shapes = Shapes(
+    extraLarge = RoundedCornerShape(100.dp),
+    large = RoundedCornerShape(10.dp),
+    medium = RoundedCornerShape(1.dp),
+)
