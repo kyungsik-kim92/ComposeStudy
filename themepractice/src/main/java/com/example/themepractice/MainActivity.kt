0f94fc381422a5cdb51282ff11e86c82b6091331
Kim Kyungsik <ksiki1@naver.com>
2023-10-20 17:54:27 +0900

    Compose <-> XML 같이 쓰기

--- themepractice/src/main/java/com/example/themepractice/MainActivity.kt
+++ themepractice/src/main/java/com/example/themepractice/MainActivity.kt
@@ -4,10 +4,12 @@ import android.content.Intent
 import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.widget.ProgressBar
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -16,10 +18,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -51,30 +57,38 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
 
+    val progressValue = remember { mutableStateOf(0) }
 
-Column {
-    Text(text = "여기는 compose1", fontSize = 40.sp)
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
 
+        AndroidView(
+            factory = {context ->
 
-    AndroidView(factory = { context ->
-        val view = LayoutInflater.from(context).inflate(R.layout.temp_xml,null,false)
+                val view = LayoutInflater.from(context).inflate(R.layout.temp_xml, null, false)
 
-        view
-    })
+                view
 
+            },
+            update = { view ->
 
+                val progressBar = view.findViewById<ProgressBar>(R.id.progressBar)
+                progressBar.progress = progressValue.value
 
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
 
+        Button(onClick = {
+            progressValue.value += 10
+        }) {
+            Text(text = "UP")
+        }
 
-
-
-    Text(text = "여기는 compose2", fontSize = 40.sp)
-
-}
-
-
-
-
+    }
 
 }
 
