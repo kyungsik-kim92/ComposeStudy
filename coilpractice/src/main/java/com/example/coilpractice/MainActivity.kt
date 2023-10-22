ecae991a80c2b45c55dd7790a9a570cb9710507e
Kim Kyungsik <ksiki1@naver.com>
2023-10-22 19:18:52 +0900

    Profile Card 만들어보기

--- coilpractice/src/main/java/com/example/coilpractice/MainActivity.kt
+++ coilpractice/src/main/java/com/example/coilpractice/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.compose.rememberImagePainter
 import com.example.coilpractice.ui.theme.ComposeStudyTheme
@@ -21,32 +31,71 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeStudyTheme {
 
-                CoilEx()
+                Column {
+                    CardEx(cardData)
+                    CardEx(cardData)
+                }
             }
+        }
+    }
+
+    companion object {
+        val cardData = CardData(
+            imageUri = "https://cdnimg.melon.co.kr/cm2/artistcrop/images/002/61/143/261143_20210325180240_org.jpg?61e575e8653e5920470a38d1482d7312/melon/optimize/90",
+            imageDescription = "아이유",
+            author = "KKS",
+            description = "아이유."
+        )
+    }
+}
+
+
+@Composable
+fun CardEx(cardData: CardData) {
+    val placeHolderColor = Color(0x33000000)
+
+
+    Card(
+
+        modifier = Modifier.padding(4.dp),
+
+
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(8.dp)
+        ) {
+
+            AsyncImage(model = cardData.imageUri, contentDescription = cardData.imageDescription,
+                modifier = Modifier.size(32.dp))
+
+            Spacer(modifier = Modifier.size(8.dp))
+            Column {
+                Text(text = cardData.author)
+                
+                Spacer(modifier = Modifier.size(4.dp))
+
+                Text(text = cardData.description)
+            }
+
         }
     }
 }
 
 
-@Composable
-fun CoilEx() {
-
-//    val painter =
-//        rememberImagePainter(data = "https://cdnimg.melon.co.kr/cm2/artistcrop/images/002/61/143/261143_20210325180240_org.jpg?61e575e8653e5920470a38d1482d7312/melon/optimize/90")
-//
-//        Image(painter = painter, contentDescription = "엔텔로프 캐년" )
-    
-    
-    
-    AsyncImage(model = "https://cdnimg.melon.co.kr/cm2/artistcrop/images/002/61/143/261143_20210325180240_org.jpg?61e575e8653e5920470a38d1482d7312/melon/optimize/90", contentDescription = "아이유")
-    
-    
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComposeStudyTheme {
-        CoilEx()
+        Row {
+            CardEx(MainActivity.cardData)
+        }
     }
 }
+
+data class CardData(
+    val imageUri: String,
+    val imageDescription: String,
+    val author: String,
+    val description: String
+)
