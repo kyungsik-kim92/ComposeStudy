5583fba67b31d35c66f44a13396134e1c54539ae
Kim Kyungsik <ksiki1@naver.com>
2023-11-16 19:31:45 +0900

    Scaffold 사용

---
+++ scaffoldpractice/src/main/java/com/example/scaffoldpractice/MainActivity.kt
@@ -0,0 +1,109 @@
+package com.example.scaffoldpractice
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.scaffoldpractice.ui.theme.ComposeStudyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeStudyTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ScaffoldEx()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CheckBoxWithContent(
+    checked: Boolean,
+    toggleState: () -> Unit,
+    content: @Composable RowScope.() -> Unit
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.clickable { toggleState() }
+    ) {
+        Checkbox(
+            checked = checked,
+            onCheckedChange = { toggleState() },
+        )
+        content()
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun ScaffoldEx() {
+    var checked by remember { mutableStateOf(false) }
+
+    Scaffold(topBar = {
+        TopAppBar(
+            navigationIcon = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Image(imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = "뒤로가기" )
+                    
+                }
+            }, title = {
+                Text(text = "Scaffold App")
+            }
+        )
+        
+
+
+    }) {
+        Surface(modifier = Modifier.padding(8.dp)) {
+            CheckBoxWithContent(checked = checked,
+                toggleState = { checked = !checked}
+            ) {
+            Text(text = "컴포즈를 좋아합니다.")
+            }
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeStudyTheme {
+        ScaffoldEx()
+    }
+}
