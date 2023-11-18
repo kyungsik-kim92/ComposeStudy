97621923c8b1ee67d3da464fd447ac016c7e8300
Kim Kyungsik <ksiki1@naver.com>
2023-11-18 19:51:54 +0900

    ConstraintLayoutEx

---
+++ constraintlayout/src/main/java/com/example/constraintlayout/MainActivity.kt
@@ -0,0 +1,94 @@
+package com.example.constraintlayout
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import com.example.constraintlayout.ui.theme.ComposeStudyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeStudyTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ConstraintLayoutEx()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ConstraintLayoutEx() {
+    ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+
+        val(redBox, magentaBox, greenBox, yellowBox) = createRefs()
+
+        Box(
+            modifier = Modifier
+                .size(40.dp)
+                .background(Color.Red)
+                .constrainAs(redBox){
+                    bottom.linkTo(parent.bottom,margin = 8.dp)
+                    end.linkTo(parent.end,margin = 4.dp)
+                }
+        )
+        Box(
+
+            modifier = Modifier
+                .size(40.dp)
+                .background(Color.Magenta)
+                .constrainAs(magentaBox){
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+
+        )
+        Box(
+
+            modifier = Modifier
+                .size(40.dp)
+                .background(Color.Green)
+                .constrainAs(greenBox){
+                    centerTo(parent)
+                }
+        )
+
+        Box(
+
+            modifier = Modifier
+                .size(40.dp)
+                .background(Color.Yellow)
+                .constrainAs(yellowBox) {
+                    start.linkTo(magentaBox.end)
+                    top.linkTo(magentaBox.bottom)
+                }
+
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ComposeStudyTheme {
+        ConstraintLayoutEx()
+    }
+}
